ade952660a525862e2d3dc892a99ebdaa5a9f017
Jay Looney <jay.m.looney@gmail.com>
2024-11-27 16:57:10 -0600

    Add partial PseudoLegality test moves, and some test-cases.

--- engine.go
+++ engine.go
@@ -141,15 +141,12 @@ func squareStringToIndex(square string) (int, int) {
 }
 
 func (board *Chessboard) MovePiece(from, to string) bool {
-
-	// Subtract ranks from 7 to invert 0-7
 	from_rank, from_file := squareStringToIndex(from)
 	to_rank, to_file := squareStringToIndex(to)
 
 	fmt.Println("Moving Piece From: ", int(from_rank), int(from_file))
 	fmt.Println("Moving Piece To: ", to_rank, to_file)
 
-	// How can I invert the ranks?
 	source_piece := board[from_rank][from_file]
 	target_piece := board[to_rank][to_file]
 
@@ -165,6 +162,76 @@ func (board *Chessboard) MovePiece(from, to string) bool {
 	*board = *newBoard
 
 	return true
+}
+
+// True if the move is psuedo legal.
+// Does not exhaustively check for checkmate, check, castle rights, etc.
+//
+// Pawns are going forward, by 1 or 2 squares, or diagonally for en passant
+// Knights are moving in L-Shapes
+// Bishops are moving Diagonally
+// Rooks are moving Orthogonally
+// Queen moves either Diagonally or Orthogonally, but not both
+// King moves only one square (or two orthogonally)
+func (board Chessboard) IsMovePsuedoLegal(from, to string) bool {
+	from_rank, from_file := squareStringToIndex(from)
+	to_rank, to_file := squareStringToIndex(to)
+
+	source_piece := board[from_rank][from_file]
+	//target_piece := board[to_rank][to_file]
+
+	// Precalculations useful for multiple piece types.
+	rank_delta := max(to_rank, from_rank) - min(to_rank, from_rank)
+	file_delta := max(to_file, from_file) - min(to_file, from_file)
+
+	switch source_piece {
+	case Empty:
+		return false
+	case WhitePawn, BlackPawn:
+		// White Pawn Rank MUST to decrease by 1 or 2
+		// Black Pawn Rank MUST to increase by 1 or 2
+		correct_forward_for_piece_type := (source_piece == WhitePawn && (to_rank < from_rank)) || (source_piece == BlackPawn && (to_rank > from_rank))
+		rank_change_in_bounds := rank_delta == 1 || rank_delta == 2
+		// Pawn file MAY increase or decrease by 1 (for en passant)
+		file_change_in_bounds := file_delta <= 1
+		return rank_change_in_bounds && file_change_in_bounds && correct_forward_for_piece_type
+	case WhiteKnight, BlackKnight:
+		rank_change_by_two := to_rank == from_rank+2 || to_rank == from_rank-2
+		file_change_by_two := to_file == from_file+2 || to_file == from_file-2
+		rank_change_by_one := to_rank == from_rank+1 || to_rank == from_rank-1
+		file_change_by_one := to_file == from_file+1 || to_file == from_file-1
+		// Rank OR file MUST change by 2 squares, and the other by 1 square.
+		return (rank_change_by_two && file_change_by_one) || (rank_change_by_one && file_change_by_two)
+	case WhiteBishop, BlackBishop:
+		// Rank AND file MUST change by an equivalent amount, and 7 at most.
+		return (rank_delta <= 7 && file_delta <= 7) && rank_delta == file_delta
+	case WhiteRook, BlackRook:
+		// TODO: Implement the cases here and below.
+		// Rank OR file MUST change by 7 at most, and the other must not change.
+		if to_rank == from_rank+1 || to_rank == from_rank-1 {
+			return true
+		}
+		fallthrough
+	case WhiteQueen, BlackQueen:
+		if to_rank == from_rank+1 || to_rank == from_rank-1 {
+			return true
+		}
+		fallthrough
+	case WhiteKing, BlackKing:
+		if to_rank == from_rank+1 || to_rank == from_rank-1 {
+			return true
+		}
+	default:
+		return false
+	}
+	return false // Default to false
+}
+
+// Returns true if the move is legal according to the rules and game state.
+// This is an exhaustive check for checkmate, check, castle rights, etc.
+// It essentially requires a review of the entire game history.
+func IsMoveStrictlyLegal(piece int, from, to string) bool {
+	return false
 }
 
 func main() {
--- engine_test.go
+++ engine_test.go
@@ -58,3 +58,41 @@ func TestSquareStringToIndex(t *testing.T) {
 		}
 	}
 }
+
+func TestIsMovePseudoLegal(t *testing.T) {
+	var cb Chessboard
+	cb.Initialize() // From the starting position.
+
+	testCases := []struct {
+		from_square string
+		to_square   string
+		expected    bool
+	}{
+		// Pawns (3 kinds of move)
+		{"d2", "d3", true},  // Pawn 1 square forward
+		{"d2", "d4", true},  // Pawn 2 squares forward
+		{"d2", "c3", true},  // Pawn diagonally forward
+		{"d2", "d2", false}, // Pawn stays in the same square
+		{"d2", "d1", false}, // Backwards
+		{"d2", "a1", false}, // Backwards and too many files away
+		{"d2", "f3", false}, // Diagnoally too many files away
+		{"d2", "d8", false}, // Forwards and too many ranks away
+		// Knights (8 move directions, 12 pseudo-legal moves from starting position)
+		// TODO: Implement more test cases for the remaining pieces below...
+		{"b1", "a3", true},
+		{"b1", "c3", true},
+		{"b1", "d2", true},
+		// Bishops (4 move directions, 8 pseudo-legal moves from starting position)
+		{"c1", "d2", true},
+		{"c1", "a3", true},
+		{"c1", "h6", true},
+	}
+
+	for _, tc := range testCases {
+		result := cb.IsMovePsuedoLegal(tc.from_square, tc.to_square)
+		if result != tc.expected {
+			t.Errorf("cb.IsMovePsuedoLegal(%s, %s) returned %t, expected %t", tc.from_square, tc.to_square, result, tc.expected)
+		}
+	}
+
+}
