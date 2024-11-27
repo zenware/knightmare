85a54f8eca4638351f9bf522823f3daee93ce260
Jay Looney <jay.m.looney@gmail.com>
2024-11-27 00:03:10 -0600

    Add Naive Piece Movement + square string -> Chessboard idx test cases

--- engine.go
+++ engine.go
@@ -70,11 +70,15 @@ func pieceToRune(piece int) rune {
 
 func (board Chessboard) Print() {
 	for rank := 0; rank <= 7; rank++ {
-		for file := 0; file < 8; file++ { // A->H
+		for file := 0; file <= 7; file++ { // A->H
 			fmt.Printf("%s ", string(pieceToRune(board[rank][file])))
 		}
+		fmt.Printf("%d", rank)
 		fmt.Println() // Newline after each rank
 	}
+	fmt.Println("---------------")
+	fmt.Println("0 1 2 3 4 5 6 7")
+	fmt.Println("a b c d e f g h")
 }
 
 // Evaluate() evaluates a chessboard and returns an advantage score.
@@ -110,9 +114,67 @@ func (board Chessboard) Evaluate() int {
 	return score
 }
 
+func runeToFile(r rune) int {
+	// A->H (from left to right)
+	file := int(r - 'a')
+	if file > 7 {
+		file -= 8
+	}
+	return file
+}
+
+// a8 = 0,0
+// a1 = 7,0
+// h1 = 7,7
+// h8 = 0,7
+func squareStringToIndex(square string) (int, int) {
+	fileRune := rune(square[0])
+	rankRune := rune(square[1])
+
+	// Subtract charpoint '0' from charpoint '0'-'9', to get the int value.
+	// https://stackoverflow.com/a/21322694
+	rankIdx := 7 - (int(rankRune-'0') - 1)
+	// Subtract ranks from 7 to invert values 0-7
+	// For Indexing Purposes, 7-7=0, 0-7=7
+	fileIdx := runeToFile(fileRune)
+	return rankIdx, fileIdx
+}
+
+func (board *Chessboard) MovePiece(from, to string) bool {
+
+	// Subtract ranks from 7 to invert 0-7
+	from_rank, from_file := squareStringToIndex(from)
+	to_rank, to_file := squareStringToIndex(to)
+
+	fmt.Println("Moving Piece From: ", int(from_rank), int(from_file))
+	fmt.Println("Moving Piece To: ", to_rank, to_file)
+
+	// How can I invert the ranks?
+	source_piece := board[from_rank][from_file]
+	target_piece := board[to_rank][to_file]
+
+	if target_piece != Empty {
+		fmt.Println("We captured a piece: ", target_piece)
+	}
+
+	board[from_rank][from_file] = Empty
+	board[to_rank][to_file] = source_piece
+
+	newBoard := board
+
+	*board = *newBoard
+
+	return true
+}
+
 func main() {
 	var cb Chessboard
 	cb.Initialize()
+	cb.Print()
+	fmt.Println("Position Evaluation: ", cb.Evaluate())
+
+	cb.MovePiece("d2", "d4")
+	cb.MovePiece("g8", "f6")
 	cb.Print()
 	fmt.Println("Position Evaluation: ", cb.Evaluate())
 }
--- engine_test.go
+++ engine_test.go
@@ -19,3 +19,42 @@ func TestChessboardEvaluate(t *testing.T) {
 		}
 	}
 }
+
+/*
+a8 = 0,0
+a1 = 7,0
+h1 = 7,7
+h8 = 0,7
+
+r n b q k b n r 0
+p p p p p p p p 1
+. . . . . . . . 2
+. . . . . . . . 3
+. . . . . . . . 4
+. . . . . . . . 5
+P P P P P P P P 6
+R N B Q K B N R 7
+0 1 2 3 4 5 6 7
+*/
+func TestSquareStringToIndex(t *testing.T) {
+	testCases := []struct {
+		square   string
+		rank_idx int
+		file_idx int
+	}{
+		{"a1", 7, 0},
+		{"a8", 0, 0},
+		{"h1", 7, 7},
+		{"h8", 0, 7},
+	}
+
+	for _, tc := range testCases {
+		result_rank_idx, result_file_idx := squareStringToIndex(tc.square)
+		if result_rank_idx != tc.rank_idx {
+			t.Errorf("squareStringToIndex(%s) returned rank_idx %d, expected rank_idx %d", tc.square, result_rank_idx, tc.rank_idx)
+		}
+		if result_file_idx != tc.file_idx {
+			t.Errorf("squareStringToIndex(%s) returned file_idx %d, expected file_idx %d", tc.square, result_file_idx, tc.file_idx)
+		}
+	}
+}
